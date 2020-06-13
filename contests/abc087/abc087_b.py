d5e18c048239e837fa99b2500885e87b1410f4a3
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-06-13 09:30:18 +0900

    abc087_b

---
+++ contests/abc087/abc087_b.py
@@ -0,0 +1,16 @@
+# B - Coins
+# https://atcoder.jp/contests/abc087/tasks/abc087_b
+
+a = int(input())
+b = int(input())
+c = int(input())
+x = int(input())
+ 
+count = 0
+for i in range(a+1):
+    for j in range(b+1):
+        for k in range(c+1):
+            if 500*i + 100*j + 50*k == x:
+                count+=1
+ 
+print(count)
