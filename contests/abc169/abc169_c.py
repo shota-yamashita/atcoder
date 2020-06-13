4d0dc72f0fa6d3236e19bd3d3916e9fb171eae50
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-06-13 19:24:13 +0900

    abc169_c.py

---
+++ contests/abc169/abc169_c.py
@@ -0,0 +1,7 @@
+# C - Multiplication 3
+# https://atcoder.jp/contests/abc169/tasks/abc169_c
+# C - Multiplication 3
+# https://atcoder.jp/contests/abc169/tasks/abc169_c
+
+A, B = input().split()
+print(int(A) * int(B.replace('.', '')) // 100)
