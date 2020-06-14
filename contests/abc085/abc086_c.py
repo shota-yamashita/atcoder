84e2844bb76a82d2a8399ea05d5a953113c74eda
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-06-14 15:51:53 +0900

    refactor abc086_c

--- contests/abc085/abc086_c.py
+++ contests/abc085/abc086_c.py
@@ -1,8 +1,6 @@
 # C - Otoshidama
 # https://atcoder.jp/contests/abc085/tasks/abc085_c
 
-import sys
- 
 N, Y = map(int, input().split())
  
 for a in range(N+1):
@@ -11,7 +9,6 @@ for a in range(N+1):
         num = a*10000 + b*5000 + c*1000
         if num == Y:
             print("{} {} {}".format(a, b, c))
-            find_ans = True
-            sys.exit()
+            exit()
  
 print('-1 -1 -1')
