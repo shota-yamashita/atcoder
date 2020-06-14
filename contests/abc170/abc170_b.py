f2de0825ae5cde267adf5c2dee164eb37dd65cd0
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-06-15 04:46:15 +0900

    abc170_b

---
+++ contests/abc170/abc170_b.py
@@ -0,0 +1,9 @@
+# B - Crane and Turtle
+# https://atcoder.jp/contests/abc170/tasks/abc170_b
+
+x, y = map(int, input().split())
+
+if y % 2 == 0 and (x * 2 <= y) and (y <= x * 4):
+    print('Yes')
+else:
+    print('No')
