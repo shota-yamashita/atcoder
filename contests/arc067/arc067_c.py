bc8e83b69c0092d434d883f3c1e2e52feed09eef
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-06-14 20:52:20 +0900

    arc067_c

---
+++ contests/arc067/arc067_c.py
@@ -0,0 +1,24 @@
+# C - Factors of Factorial
+# https://atcoder.jp/contests/arc067/tasks/arc067_a
+
+import math
+
+N=math.factorial(int(input()))
+
+i = 2
+ans = 1
+
+M = 10 ** 9 + 7
+
+while i * i <= N:
+    cnt = 1
+    while N % i == 0:
+        cnt += 1
+        N //= i
+    ans *= cnt
+    i += 1
+
+if N != 1:
+    ans *= 2
+
+print(int(ans%M))
