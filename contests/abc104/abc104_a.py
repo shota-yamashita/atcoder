9357e1f69e49fcb738def7d9c84440921e7a7071
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-06-20 07:10:01 +0900

    abc104_a

---
+++ contests/abc104/abc104_a.py
@@ -0,0 +1,10 @@
+# A - Rated for Me
+# https://atcoder.jp/contests/abc104/tasks/abc104_a
+
+r = int(input())
+if r < 1200:
+    print('ABC')
+elif r < 2800:
+    print('ARC')
+else:
+    print('AGC')
