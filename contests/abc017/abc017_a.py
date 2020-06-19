943d0a66c5489b8acf5d490f55fc67409fb29daf
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-06-20 05:43:45 +0900

    abc017_a

---
+++ contests/abc017/abc017_a.py
@@ -0,0 +1,7 @@
+# A - プロコン
+# https://atcoder.jp/contests/abc017/tasks/abc017_1
+
+s1,e1=map(int,input().split())
+s2,e2=map(int,input().split())
+s3,e3=map(int,input().split())
+print((s1*e1+s2*e2+s3*e3)//10)
