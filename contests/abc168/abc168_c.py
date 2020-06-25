95d91ed2ca0393867bf6cfc20fd8b523554d7335
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-06-26 05:27:42 +0900

    abc168_c

---
+++ contests/abc168/abc168_c.py
@@ -0,0 +1,12 @@
+# C - : (Colon)
+# https://atcoder.jp/contests/abc168/tasks/abc168_c
+
+import math
+ 
+A, B, H, M = map(int, input().split())
+ 
+th_h = 360 // 12 * H + 360 / 12 / 60 * M
+th_m = 360 // 60 * M
+ 
+ans = A ** 2 + B ** 2 - 2*A*B*math.cos(math.radians(abs(th_h - th_m)))
+print(math.sqrt(ans))
