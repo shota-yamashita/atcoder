728d969017deb54cf2ef49e8d9a6b50c7af7de2a
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-07 05:24:00 +0900

    abc085_a

---
+++ contests/abc085/abc085_a.cpp
@@ -0,0 +1,13 @@
+// A - Already 2018
+// https://atcoder.jp/contests/abc085/tasks/abc085_a
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    cout << "2018" << s.substr(4) << endl;
+}
