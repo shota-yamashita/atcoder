3eadd1a96d577cf5da974087416d200a3b879f23
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-07 05:35:01 +0900

    abc069_b.cpp

---
+++ contests/abc069/abc069_b.cpp
@@ -0,0 +1,13 @@
+// B - i18n
+// https://atcoder.jp/contests/abc069/tasks/abc069_b
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    cout << "2018" << s.substr(4) << endl;
+}
