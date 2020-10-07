21351efec8f9e3ac72e97e0ca94ebd3814dfba71
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-08 06:13:48 +0900

    abc174_a

---
+++ contests/abc174/abc174_a.cpp
@@ -0,0 +1,10 @@
+// A - Air Conditioner
+// https://atcoder.jp/contests/abc174/tasks/abc174_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x; cin >> x;
+    cout << (x>= 30 ? "Yes" : "No") << endl;
+}
