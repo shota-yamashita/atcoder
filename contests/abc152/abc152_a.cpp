1594f8f44aef74697a81589cca2b87c74829b5fa
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-09 08:13:35 +0900

    abc152_a

---
+++ contests/abc152/abc152_a.cpp
@@ -0,0 +1,10 @@
+// A - AC or WA
+// https://atcoder.jp/contests/abc152/tasks/abc152_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m; cin >> n >> m;
+    cout << ((n == m) ? "Yes" : "No") << endl;
+}
