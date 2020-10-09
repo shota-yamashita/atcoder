c0fece2b46384f5007603334ef8fe2e438ab16d5
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-10 06:03:33 +0900

    abc151_a

---
+++ contests/abc151/abc151_a.cpp
@@ -0,0 +1,10 @@
+// A - Next Alphabet
+// https://atcoder.jp/contests/abc151/tasks/abc151_a
+
+#include <stdio.h>
+int main() {
+    char x;
+    scanf("%c", &x);
+    printf("%c", x+1);
+    return 0;
+}
