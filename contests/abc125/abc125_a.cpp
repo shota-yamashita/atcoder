c99585fd6beeca3eae4ec3b15f17e387656ebc32
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-10 19:40:01 +0900

    abc125_a

---
+++ contests/abc125/abc125_a.cpp
@@ -0,0 +1,10 @@
+// A - Biscuit Generator
+// https://atcoder.jp/contests/abc125/tasks/abc125_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, t; cin >> a >> b >> t;
+    cout << t/a*b << endl;
+}
