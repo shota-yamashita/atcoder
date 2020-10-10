651f56a54b2ff8babb3421652330d8d5c43b63fb
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-10 17:25:13 +0900

    abc138_a.cpp

---
+++ contests/abc138/abc138_a.cpp
@@ -0,0 +1,12 @@
+// A - Red or Not
+// https://atcoder.jp/contests/abc138/tasks/abc138_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a; cin >> a;
+    string s; cin >> s;
+    if(3200<=a) cout << s << endl;
+    else cout << "red" << endl;
+}
