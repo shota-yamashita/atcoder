0a5d406dde1a0dec21c48cb1a6fc273411d7f4c3
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-10 18:16:57 +0900

    abc128_a.cpp

---
+++ contests/abc128/abc128_a.cpp
@@ -0,0 +1,10 @@
+// A - Apple Pie
+// https://atcoder.jp/contests/abc128/tasks/abc128_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, p; cin >> a >> p;
+    cout << (a*3+p)/2 << endl;
+}
