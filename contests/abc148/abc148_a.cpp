861fae551ad2afffe65fbb147c054008ff1e27c2
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-10 15:28:52 +0900

    abc148_a.cpp

---
+++ contests/abc148/abc148_a.cpp
@@ -0,0 +1,11 @@
+// A - Round One
+// https://atcoder.jp/contests/abc148/tasks/abc148_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a; cin >> a;
+    int b; cin >> b;
+    cout << 6-a-b << endl;
+}
