c2c298b73ea1599b2c6b9dc15ee7f36f535191f4
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-10 18:07:48 +0900

    abc130_a

---
+++ contests/abc130/abc130_a.cpp
@@ -0,0 +1,11 @@
+// A - Rounding
+// https://atcoder.jp/contests/abc130/tasks/abc130_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, a; cin >> x >> a;
+    if(x<a) cout << 0 << endl;
+    else cout << 10 << endl;
+}
