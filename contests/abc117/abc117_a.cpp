4d2efd70c309e27482a56722c91d19b3ef5acfc8
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-11 16:40:24 +0900

    abc117_a

---
+++ contests/abc117/abc117_a.cpp
@@ -0,0 +1,10 @@
+// A - Entrance Examination
+// https://atcoder.jp/contests/abc117/tasks/abc117_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double t, x; cin >> t >> x;
+    cout << t/x << endl;
+}
