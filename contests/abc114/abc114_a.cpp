5abcddcb77b634f2a7ce7d2ad9dae5e9d5ca04c6
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-12 06:10:46 +0900

    abc114_a

---
+++ contests/abc114/abc114_a.cpp
@@ -0,0 +1,10 @@
+// A - 753
+// https://atcoder.jp/contests/abc114/tasks/abc114_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x; cin >> x;
+    cout<<(x==3||x==5||x==7 ? "YES" : "NO" )<<endl;
+}
