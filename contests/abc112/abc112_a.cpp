8eeb6f2d7aa85c886e5a250c2dd7437483cd9855
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-12 06:19:52 +0900

    abc112_a

---
+++ contests/abc112/abc112_a.cpp
@@ -0,0 +1,15 @@
+// A - Programming Education
+// https://atcoder.jp/contests/abc112/tasks/abc112_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    if(n==1) {
+        cout << "Hello World" << endl;
+    } else {
+        int a, b; cin >> a >> b;
+        cout << a+b << endl;
+    }
+}
