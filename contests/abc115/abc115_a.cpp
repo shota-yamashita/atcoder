270e9f8dcbd5e235c4ed9d2f4cbf08b502e451e0
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-12 06:05:14 +0900

    abc115_a

---
+++ contests/abc115/abc115_a.cpp
@@ -0,0 +1,13 @@
+// A - Christmas Eve Eve Eve
+// https://atcoder.jp/contests/abc115/tasks/abc115_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int d; cin >> d;
+    if(d==25) cout << "Christmas" << endl;
+    else if(d==24) cout << "Christmas Eve" << endl;
+    else if (d==23) cout << "Christmas Eve Eve" << endl;
+    else if (d==22) cout << "Christmas Eve Eve Eve" << endl;
+}
