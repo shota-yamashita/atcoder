40233233d557014e4f8a647d31e944ae3c2c01db
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-12 06:17:30 +0900

    abc113_a

---
+++ contests/abc113/abc113_a.cpp
@@ -0,0 +1,10 @@
+// A - Discount Fare
+// https://atcoder.jp/contests/abc113/tasks/abc113_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y; cin >> x >> y;
+    cout << x+y/2 << endl;
+}
