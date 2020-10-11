5c604efc892f6caafbf4d43d9ed87ac231c67729
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-11 16:31:43 +0900

    abc119_a

---
+++ contests/abc119/abc119_a.cpp
@@ -0,0 +1,11 @@
+// A - Still TBD
+// https://atcoder.jp/contests/abc119/tasks/abc119_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s; cin >> s;
+    if(s<="2019/04/30") cout << "Heisei" << endl;
+    else cout << "TBD" << endl;
+}
