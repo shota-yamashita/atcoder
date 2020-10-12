dae59d845f1b28ce38fe853eaf61b73e45bc0295
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-12 22:10:41 +0900

    abc108_a

---
+++ contests/abc108/abc108_a.cpp
@@ -0,0 +1,10 @@
+// A - Something on It 
+// https://atcoder.jp/contests/abc095/tasks/abc095_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int k; cin >> k;
+    cout << (k/2 + k%2) * (k/2) << endl;
+}
