99323ae7920b8a3a285792420bcfa6bb83348121
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-13 04:56:45 +0900

    abc105_a

---
+++ contests/abc105/abc105_a.cpp
@@ -0,0 +1,11 @@
+// A - AtCoder Crackers
+// https://atcoder.jp/contests/abc105/tasks/abc105_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k; cin >> n >> k;
+    if (n%k==0) cout << 0 << endl;
+    else cout << 1 << endl;
+}
