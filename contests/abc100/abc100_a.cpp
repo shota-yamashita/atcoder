86e9e53dd0a7dabe08f39e372dc89aebadb28338
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-13 05:19:50 +0900

    abc100_a

---
+++ contests/abc100/abc100_a.cpp
@@ -0,0 +1,11 @@
+// A - Happy Birthday!
+// https://atcoder.jp/contests/abc100/tasks/abc100_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b; cin >> a >> b;
+    if(a<=8&&b<=8) cout << "Yay!" << endl;
+    else cout << ":(" << endl;
+}
