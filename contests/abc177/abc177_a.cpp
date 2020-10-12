70d575bd1e3fa40f20c076bac9be100ed2b962e0
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-13 07:16:27 +0900

    abc177_a

---
+++ contests/abc177/abc177_a.cpp
@@ -0,0 +1,11 @@
+// A - Don't be late
+// https://atcoder.jp/contests/abc177/tasks/abc177_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int d, t, s; cin >> d >> t >> s;
+    if(t*s>=d) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
