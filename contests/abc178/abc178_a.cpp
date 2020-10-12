341188afa2170d126921e8b4bd4b36af81b8ce10
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-13 07:18:25 +0900

    abc178_a

---
+++ contests/abc178/abc178_a.cpp
@@ -0,0 +1,11 @@
+// A - Not
+// https://atcoder.jp/contests/abc178/tasks/abc178_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x; cin >> x;
+    if(x==0) cout << 1 << endl;
+    else cout << 0 << endl;
+}
