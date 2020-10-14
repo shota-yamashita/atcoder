26b817424d0bc028ffc8dbaeb8cc351c6c32830b
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-14 20:54:28 +0900

    abc096_a

---
+++ contests/abc096/abc096_a.cpp
@@ -0,0 +1,11 @@
+// A - Day of Takahashi
+// https://atcoder.jp/contests/abc096/tasks/abc096_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b; cin >> a >> b;
+    if(a<=b) cout << a << endl;
+    else cout << a-1 << endl;
+}
