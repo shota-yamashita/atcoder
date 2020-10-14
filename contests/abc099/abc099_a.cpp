ee51e1503e981ec374afc9f8e37e3341f047ee4a
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-14 20:46:26 +0900

    abc099_a

---
+++ contests/abc099/abc099_a.cpp
@@ -0,0 +1,11 @@
+// A - ABD
+// https://atcoder.jp/contests/abc099/tasks/abc099_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    if (n <= 999) cout << "ABC" << endl;
+    else cout << "ABD" << endl;
+}
