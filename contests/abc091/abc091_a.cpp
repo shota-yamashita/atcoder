382c37d0fa4ddc31866927667370e173a9878f0d
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-16 08:30:27 +0900

    abc091_a

---
+++ contests/abc091/abc091_a.cpp
@@ -0,0 +1,11 @@
+// A - Traveling Budget
+// https://atcoder.jp/contests/abc092/tasks/abc092_a
+
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int a, b, c; cin>>a>>b>>c;
+    if(a+b>=c) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
