f8a610fb75082af4443f4a117986ed5dffd7c101
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-16 08:41:30 +0900

    abc090_a

---
+++ contests/abc090/abc090_a.cpp
@@ -0,0 +1,10 @@
+// A - Diagonal String
+// https://atcoder.jp/contests/abc090/tasks/abc090_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str1, str2, str3; cin>>str1>>str2>>str3;
+    cout << str1[0] << str2[1] << str3[2] << endl;
+}
