18589d591d2cb99c4a927ba72cc76a17966d8a0c
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-16 08:44:27 +0900

    abc089_a

---
+++ contests/abc089/abc089_a.cpp
@@ -0,0 +1,10 @@
+// A - Grouping 2
+// https://atcoder.jp/contests/abc089/tasks/abc089_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    cout << n/3 << endl;
+}
