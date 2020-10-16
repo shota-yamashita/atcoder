52d54b3dad09b3ced974edfafbd2557d96131b27
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-16 21:10:37 +0900

    abc082_a

---
+++ contests/abc082/abc082_a.cpp
@@ -0,0 +1,10 @@
+// A - Round Up the Mean
+// https://atcoder.jp/contests/abc082/tasks/abc082_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b; cin>>a>>b;
+    cout << (a+b+1)/2;
+}
