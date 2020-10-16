546e42e5165b9bfd27f6b4f370800c89837ffda9
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-17 07:23:03 +0900

    abc080_a

---
+++ contests/abc080/abc080_a.cpp
@@ -0,0 +1,10 @@
+// A - Parking
+// https://atcoder.jp/contests/abc080/tasks/abc080_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a, b;cin>>n>>a>>b;
+    cout << min(a*n, b) << endl;
+}
