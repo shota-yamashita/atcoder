226229a640116222f9451b3a8888211f5e8b3d4d
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-16 20:57:07 +0900

    abc087_a

---
+++ contests/abc087/abc087_a.cpp
@@ -0,0 +1,10 @@
+// A - Buying Sweets
+// https://atcoder.jp/contests/abc087/tasks/abc087_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, a, b; cin>>x>>a>>b;
+    cout << (x-a)%b << endl;
+}
