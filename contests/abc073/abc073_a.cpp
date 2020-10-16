02041aebe550bc62d61a05ad6704091452e378f1
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2020-10-17 07:37:01 +0900

    abc073_a

---
+++ contests/abc073/abc073_a.cpp
@@ -0,0 +1,11 @@
+// A - September 9
+// https://atcoder.jp/contests/abc073/tasks/abc073_a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s; cin >> s;
+    if(s[0]=='9' || s[1]=='9') cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
