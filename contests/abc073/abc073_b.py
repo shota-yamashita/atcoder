5611adfeadeb81e5e012f6c87d98f491a7089b80
shota-yamashita <janpin99px.dreikbm3.x@gmail.com>
2021-07-03 19:30:18 +0900

    update abc073_b

--- contests/abc073/abc073_b.py
+++ contests/abc073/abc073_b.py
@@ -1,4 +1,11 @@
 # B - Theater
 # https://atcoder.jp/contests/abc073/tasks/abc073_b
 
-print(sum(1-eval(input().replace(" ","-")) for _ in range(int(input()))))
+n = int(input())
+
+people = 0
+for i in range(n):
+    l, r = map(int,input().split())
+    people += r - l + 1
+
+print(people)
